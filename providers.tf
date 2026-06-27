ad745c1b1a6677d1ffcb876ddc25e82aafeda083
Rana Pratap Singh <rana.singh15720@gmail.com>
2026-06-27 18:17:16 +0530

    fix duplicate provider block

--- providers.tf
+++ providers.tf
@@ -1,8 +1,8 @@
 terraform {
   required_providers {
-    oci = { source = "oracle/oci" }
+    oci = {
+      source  = "oracle/oci"
+      version = ">= 8.0.0"
+    }
   }
-}
-provider "oci" {
-  region = "ap-mumbai-1"
 }
